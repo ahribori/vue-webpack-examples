0d84b75c4fc30645b57fc72071dbd213ab3c2a5d
ahribori <ahribori@gmail.com>
2018-05-13 01:19:00 +0900

    Add counter page with vuex applied

--- src/store.js
+++ src/store.js
@@ -4,7 +4,27 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    count: 0,
+    test: {
+      a: 3,
+      b: {
+        c: 7,
+      }
+    }
+  },
+  mutations: {
+    increment: (state, payload) => {
+      state.count += payload.amount;
+    },
+  },
+  actions: {
+    incrementAction: ({commit}, payload) => {
+      setTimeout(() => {
+        commit('increment', payload);
+      }, 1000);
+
+    },
+  },
+  getters: {}
 });
---
+++ src/views/Counter.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="counter">
+    <h1>This is a counter page</h1>
+    <h3>counter : {{ count }}</h3>
+    <button v-on:click="onIncreaseButtonClicked">increase</button>
+    <button v-on:click="onIncreaseActionButtonClicked">increase by action</button>
+  </div>
+</template>
+
+<script>
+  import store from '../store';
+
+  export default {
+    name: "Counter",
+    computed: {
+      count: () => store.state.count
+
+    },
+    methods: {
+      onIncreaseButtonClicked: () => {
+        store.commit('increment', {
+          amount: 1
+        });
+      },
+      onIncreaseActionButtonClicked: () => {
+        store.dispatch('incrementAction', {
+          amount: 1
+        });
+      }
+    }
+  }
+</script>
