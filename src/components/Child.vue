b42ff8bc43bcb907c8bc04b689a85b5c09ad5646
ahribori <ahribori@gmail.com>
2018-05-17 00:50:29 +0900

    변수를 반응형으로 만드려면 초기화를 해야한다!

--- src/components/Child.vue
+++ src/components/Child.vue
@@ -2,6 +2,9 @@
   <div>
     <h2>Child</h2>
     <p>{{message}}</p>
+    <p>{{a}}</p>
+    <p>{{b}}</p>
+    <button v-on:click="handleClick">뷰모델에 속성 추가</button>
   </div>
 </template>
 
@@ -10,6 +13,18 @@
     name: "Child",
     props: {
       message: String,
+    },
+    data: function () {
+      return {
+        a: 1,
+        b: '', // b라는 변수를 반응형으로 만들려면 뭐라도 선언 해놔야함.
+      };
+    },
+    methods: {
+      handleClick: function (){
+        this.$data.a ++;
+        this.$data.b = 3;
+      },
     }
   }
 </script>
